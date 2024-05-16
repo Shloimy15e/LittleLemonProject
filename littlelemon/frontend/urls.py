4cde5f47251d576123bc141ccaa0fe8457744abc
Shloimy <shloimyelbaum@gmail.com>
2024-05-16 17:00:39 +0300

    set up the basic static frontend using provided assets and templates (from other project)

--- littlelemon/frontend/urls.py
+++ littlelemon/frontend/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from django.urls import include
 
+from .views import IndexView
+from .views import AboutView
+from .views import MenuView
+from .views import BookView
+from .views import BookingsView
+
 urlpatterns = [
-    path()
+    path('', IndexView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('menu/', MenuView.as_view(), name='menu'),
+    path('book/', BookView.as_view(), name='book'),
+    path('reservations/', BookingsView.as_view(), name='reservations'),
 ]
--- littlelemon/frontend/views.py
+++ littlelemon/frontend/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 from django.views.generic import TemplateView 
 
 # Create your views here.
-class HomeView(TemplateView):
-      template_name = 'home.html'
+class IndexView(TemplateView):
+      template_name = 'index.html'
+      
+class AboutView(TemplateView):
+      template_name = 'about.html'
+      
+class MenuView(TemplateView):
+      template_name = 'menu.html'
+      
+class BookView(TemplateView):
+      template_name = 'book.html'
+      
+class BookingsView(TemplateView):
+      template_name = 'bookings.html'
