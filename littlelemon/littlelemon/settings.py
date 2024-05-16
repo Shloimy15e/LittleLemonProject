e677a23bbb3ded1b4e43c3fa385d88b44633389d
Shloimy <shloimyelbaum@gmail.com>
2024-05-16 16:28:42 +0300

    added the apps booking_api, booking_web, menu_api, and menu_web to the project. and set up the settings.py file for the apps and for mysql db.

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'booking_api',
+    'menu_api',
 ]
 
 MIDDLEWARE = [
