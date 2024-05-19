2e6abe12d6394bb681005dc0f032ded3d40f468a
Shloimy <shloimyelbaum@gmail.com>
2024-05-19 18:10:36 +0300

    almost finished book.html page. going back to booking model to modify the time field to be 0:00 instead of 00:00:00. will also add image field to menu model.

--- littlelemon/frontend/static/js/fetch_bookings.js
+++ littlelemon/frontend/static/js/fetch_bookings.js
@@ -1,8 +1,7 @@
 const date = document.querySelector('#date');
 const reservationDate = document.querySelector('#reservation_date');
 
-reservationDate.addEventListener('change', function() {
-});
+reservationDate.addEventListener('change', getBookings);
 
 // Define the get bookings function
 function getBookings(){
@@ -20,6 +19,7 @@ function getBookings(){
             // Get the bookings element
             const bookingHeading = document.querySelector('#booking-heading');
             const bookings = document.querySelector('#bookings');
+            const reservationSlot = document.querySelector('#reservation_slot');
             // Clear the bookings elements
             bookingHeading.textContent = 'Bookings for ' + selectedDate;
             bookings.innerHTML = '';
@@ -28,6 +28,7 @@ function getBookings(){
                   bookingHeading.textContent = 'No bookings for this date. You can be the first!';
                   return;
             }
+            timeSlots = []
             // Loop through the bookings
             data.results.forEach(booking => {
                   // Create a new elements for name and time
@@ -40,14 +41,27 @@ function getBookings(){
                   span.className = 'booking-time';
                   // Append the p element to the bookings element
                   bookings.appendChild(p);
+                  bookings.appendChild(span);
+                  timeSlots += booking.time;
             });
             // If everything is successful, then add time slots which are not booked
-            const timeSlots = ['12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00'];
-            const reservationSlot = document.querySelector('#reservation_slot');
+            slot_options = reservationSlot.innerHTML;
             reservationSlot.innerHTML = '';
             // Loop through the bookings returned from the API and see if the time slot is booked
-            
-            
+            for(let i = 9; i <  21; i += 0.5){
+                  const formattedTime = formatTime(i);
+                  if(timeSlots.includes(i)){
+                        slot_options +=  `<option value="${formattedTime}" disabled>${formattedTime}</option>`
+                  }else{
+                        slot_options += `<option value="${formattedTime}">${formattedTime}</option>`
+                  }
+            }      
+            reservationSlot.innerHTML = slot_options;     
+            function formatTime(hours) {
+                  const date = new Date(0);
+                  date.setSeconds(hours * 3600); // convert hours to seconds
+                  return date.toISOString().substr(11, 8);
+            } 
       })
       .catch(error => {
             console.error('There has been a problem with your fetch operation:', error);
